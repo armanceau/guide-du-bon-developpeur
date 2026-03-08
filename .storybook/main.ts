0022521f24c6ef24d6cdfee52a54a6593b04fc44
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2026-03-08 19:47:38 +0100

    fix(): fix error ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-docs",
     "@chromatic-com/storybook",
@@ -15,7 +12,6 @@ const config: StorybookConfig = {
     options: {},
   },
   docs: {
-    autodocs: "tag",
     defaultName: "Documentation",
   },
 };
