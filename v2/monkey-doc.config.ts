504924f7f4f00c3d90e8557727c22adad333c16e
armanceau <102350268+armanceau@users.noreply.github.com>
2026-06-14 16:46:05 +0200

    update

---
+++ v2/monkey-doc.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'monkey-doc';
+
+export default defineConfig({
+  title: 'My Documentation',
+  description: 'Built with Monkey-Doc',
+  // github: 'https://github.com/your-org/your-repo',
+  // logo: '/logo.svg',
+  // docsDir: 'docs',
+});
